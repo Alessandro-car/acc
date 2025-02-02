46754694f3f3a1d4290af2ff2ae09d5d818b55b9
ale_carella <useradd@DESKTOP-U4GSC2L>
2025-02-02 21:45:58 +0100

    I fixed the lexer so it can lex any string written in C language without comments

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,43 +1,76 @@
+static KEYWORDS: [&str; 34] = [
+   "auto", "break", "case", "char", "const", "continue",
+   "default", "do", "double",  "else", "enum", "extern",
+   "float", "for", "goto", "if", "inline", "int",
+   "long", "register", "restrict", "return", "short", "signed",
+   "sizeof", "static", "struct", "switch", "typedef", "union",
+   "unsigned", "void", "volatile", "while"
+];
+
+static OPERATORS: [&str; 37] = [
+    "+", "-", "*", "/", "%", "++", "--", //Arithmetic operators
+    "==", "!=", ">", "<", ">=", "<=", //Relational operators
+    "&&", "||", "!", //Logical operators
+    "&", "|", "^", "~", "<<", ">>", //Bitwise operators
+    "=", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=", //Assignment operators
+    "?:", ".", "->", "&" //Other operators
+];
+
+static PREPROCESSOR_DIR: [&str; 11] = [
+    "#include", "#define", "#undef", "#ifdef", "#ifndef",
+    "#if", "#else", "#elif", "#endif", "#error", "#pragma"
+];
+
 #[derive(PartialEq)]
 #[derive(Debug)]
-pub enum tok_type {
-    ILLEGAL,
+pub enum TokType {
     EOF,
-    IDENTIFIER(Vec<char>),
-    INT(Vec<char>),
-    ASSIGN(char),
-    PLUS(char),
-    COMMA(char),
-    SEMICOLON(char),
+    ILLEGAL,
+    IDENTIFIER(String),
+    NUMBER(String),
+    OPERATOR(String),
     LPAREN(char),
     RPAREN(char),
     LBRACE(char),
     RBRACE(char),
-    FUNCTION,
-    LET,
-    TRUE,
-    FALSE,
-    IF,
-    ELSE,
-    RETURN,
-    MINUS(char),
-    BANG(char),
-    ASTERISK(char),
-    SLASH(char),
-    LT(char),
-    GT(char)
+    LSQUARE(char),
+    RSQUARE(char),
+    SEMICOLON(char),
+    COMMA(char),
+    KEYWORD(String),
+    COMMENT(String),
+    PREPROCESSOR(String),
 }
 
-fn get_keyword_token(ident: &Vec<char>) -> Result<tok_type, String> {
+fn get_keyword_token(ident: &Vec<char>) -> Result<TokType, String> {
     let identifier: String = ident.into_iter().collect();
-    match &identifier[..] {
-        "true" => Ok(tok_type::TRUE),
-        "false" => Ok(tok_type::FALSE),
-        "if" => Ok(tok_type::IF),
-        "else" => Ok(tok_type::ELSE),
-        "return" => Ok(tok_type::RETURN),
-        _ => Err(String::from("Not a keyword"))
+    for keyword in KEYWORDS.iter() {
+        if keyword.to_string() == identifier {
+            return Ok(TokType::KEYWORD(identifier))
+        }
     }
+
+    Err(String::from("Not a keyword"))
+}
+
+fn get_preprocessor_dir(ident: &Vec<char>) -> Result<TokType, String> {
+    let identifier: String = ident.into_iter().collect();
+    for preprocessor_dir in PREPROCESSOR_DIR.iter() {
+        if identifier == preprocessor_dir.to_string() {
+            return Ok(TokType::PREPROCESSOR(identifier))
+        }
+    }
+    Err(String::from("Not a preprocessor dir"))
+}
+
+fn get_operator_token(ident: &Vec<char>) -> Result<TokType, String> {
+    let identifier: String = ident.into_iter().collect();
+    for operator in OPERATORS.iter() {
+        if identifier == operator.to_string() {
+            return Ok(TokType::OPERATOR(identifier));
+        }
+    }
+    Err(String::from("Not an operator"))
 }
 
 fn is_letter(ch: char) -> bool {
@@ -58,16 +91,16 @@ pub struct Lexer {
 impl Lexer {
     pub fn new(input: Vec<char>) -> Self {
         Self {
-            input: input,
+            input,
             position: 0,
             read_position: 0,
-            ch: '0'
+            ch: '$'
         }
     }
 
     pub fn read_char(&mut self) {
         if self.read_position >= self.input.len() {
-            self.ch = '0';
+            self.ch = '$';
         } else {
             self.ch = self.input[self.read_position];
         }
@@ -82,7 +115,7 @@ impl Lexer {
         }
     }
 
-    pub fn next_token(&mut self) -> tok_type {
+    pub fn next_token(&mut self) -> TokType {
         let read_identifier = |l: &mut Lexer| -> Vec<char> {
             let position = l.position;
             while l.position < l.input.len() && is_letter(l.ch) {
@@ -99,25 +132,55 @@ impl Lexer {
             l.input[position..l.position].to_vec()
         };
 
-        let token: tok_type;
+        let read_preprocessor_dir = |l: &mut Lexer| -> Vec<char> {
+            let position = l.position;
+            while l.position < l.input.len() && is_letter(l.ch) || l.ch == '#' {
+                l.read_char();
+            }
+            l.input[position..l.position].to_vec()
+        };
+
+        let read_operator = |l: &mut Lexer| -> Vec<char> {
+            let position = l.position;
+            while l.position < l.input.len() && !is_letter(l.ch) && !is_digit(l.ch) && l.ch != '#' && l.ch != ' '{
+                l.read_char();
+            }
+            l.input[position..l.position].to_vec()
+        };
+
+        let token: TokType;
         self.skip_whitespace();
         match self.ch {
-            '=' => token = tok_type::ASSIGN(self.ch),
-            '+' => token = tok_type::PLUS(self.ch),
-            '-' => token = tok_type::MINUS(self.ch),
-            '!' => token = tok_type::BANG(self.ch),
-            '/' => token = tok_type::SLASH(self.ch),
-            '*' => token = tok_type::ASTERISK(self.ch),
-            '<' => token = tok_type::LT(self.ch),
-            '>' => token = tok_type::GT(self.ch),
-            ';' => token = tok_type::SEMICOLON(self.ch),
-            '(' => token = tok_type::LPAREN(self.ch),
-            ')' => token = tok_type::RPAREN(self.ch),
-            ',' => token = tok_type::COMMA(self.ch),
-            '{' => token = tok_type::LBRACE(self.ch),
-            '}' => token = tok_type::RBRACE(self.ch),
-            '0' => token = tok_type::EOF,
+            '$' => token = TokType::EOF,
+            '(' => token = TokType::LPAREN(self.ch),
+            ')' => token = TokType::RPAREN(self.ch),
+            '{' => token = TokType::LBRACE(self.ch),
+            '}' => token = TokType::RBRACE(self.ch),
+            '[' => token = TokType::LSQUARE(self.ch),
+            ']' => token = TokType::RSQUARE(self.ch),
+            ';' => token = TokType::SEMICOLON(self.ch),
+            ',' => token = TokType::COMMA(self.ch),
             _ => {
+                if !is_letter(self.ch) && !is_digit(self.ch) && self.ch != '#' && self.ch != ' '{
+                    let operator: Vec<char> = read_operator(self);
+                    match get_operator_token(&operator) {
+                        Ok(operator_token) => {
+                            return operator_token;
+                        }
+                        Err(_err) => {}
+                    }
+                }
+
+                if self.ch == '#' {
+                    let ident: Vec<char> = read_preprocessor_dir(self);
+                    match get_preprocessor_dir(&ident) {
+                        Ok(preprocessor_token) => {
+                            return preprocessor_token;
+                        }
+                        Err(_err) => {},
+                    }
+                }
+
                 if is_letter(self.ch) {
                     let ident: Vec<char> = read_identifier(self);
                     match get_keyword_token(&ident) {
@@ -125,19 +188,18 @@ impl Lexer {
                             return keyword_token;
                         },
                         Err(_err) => {
-                            return tok_type::IDENTIFIER(ident);
+                            return TokType::IDENTIFIER(ident.into_iter().collect());
                         }
                     }
                 } else if is_digit(self.ch) {
-                    let ident: Vec<char> = read_number(self);
-                    return tok_type::INT(ident);
-                } else {
-                    return tok_type::ILLEGAL
+                    let ident: String = read_number(self).into_iter().collect();
+                    return TokType::NUMBER(ident);
                 }
+
+                return TokType::ILLEGAL
             }
         }
         self.read_char();
         token
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod lexer;
 
 fn main() {
-    let input: String = String::from("if { } else { }");
+    let input: String = String::from("#include int a = 5;");
     let mut lexer = lexer::Lexer::new(input.chars().collect());
     lexer.read_char();
     loop {
         let token = lexer.next_token();
-        if token == lexer::tok_type::EOF {
+        if token == lexer::TokType::EOF {
             break;
         } else {
             println!("{:?}", token);
